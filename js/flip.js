b8c18a834b5547b94248eae0f858d7d2143b6373
Virgile Bigaré <100876844+virgill-e@users.noreply.github.com>
2023-08-19 00:31:29 +0200

    Update flip.js supp affichage console

--- js/flip.js
+++ js/flip.js
@@ -2,6 +2,5 @@ function flip () {
     //todo:Lors du ckick  sur container la div card va tourner en recevent ou en perdant la classe flipped
     var element = document.getElementById("flip");
     element.classList.toggle("flipped");
-    console.log("flip");
 }
 
