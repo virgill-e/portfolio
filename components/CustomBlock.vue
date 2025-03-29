6a7d1286351a6236e0a60f328ffdcaf69b32dbdc
virgile <virgile.bigare@gmail.com>
2025-03-29 12:04:45 +0100

    feat: enhance CustomBlock component layout and styling for better responsiveness

--- components/CustomBlock.vue
+++ components/CustomBlock.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="p-6 rounded-lg shadow-md" :class="bgColorClass">
-    <div class="flex items-center mb-4">
-      <div class="w-10 h-10 mr-3" :class="textColorClass" v-html="svgContent"></div>
-    </div>
+  <div class="p-6 rounded-lg shadow-md flex justify-center items-center" :class="bgColorClass">
+    <div class="w-16 h-16 flex justify-center items-center svg-container" :class="textColorClass" v-html="svgContent"></div>
   </div>
 </template>
 
@@ -25,3 +23,10 @@ const props = defineProps({
 const bgColorClass = computed(() => `${props.bgColor}`);
 const textColorClass = computed(() => `${props.textColor}`);
 </script>
+
+<style scoped>
+.svg-container :deep(svg) {
+  width: 100%;
+  height: 100%;
+}
+</style>
