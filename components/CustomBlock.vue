6e04791a12b739e8c6772d331d877462b0afd5e2
virgile <virgile.bigare@gmail.com>
2025-03-29 11:38:59 +0100

    feat: add CustomBlock component and integrate it into the main page

---
+++ components/CustomBlock.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="p-6 rounded-lg shadow-md" :class="bgColorClass">
+    <div class="flex items-center mb-4">
+      <div class="w-10 h-10 mr-3" :class="textColorClass" v-html="svgContent"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  bgColor: {
+    type: String,
+    default: 'bg-cream'
+  },
+  textColor: {
+    type: String,
+    default: 'text-forest'
+  },
+  svgContent: {
+    type: String,
+    required: true
+  },
+});
+
+const bgColorClass = computed(() => `${props.bgColor}`);
+const textColorClass = computed(() => `${props.textColor}`);
+</script>
