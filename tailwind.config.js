e48ad828525ea1d130075faebc1fb7d1be83e101
virgile <virgile.bigare@gmail.com>
2025-03-30 22:03:22 +0200

    feat: add social media SVG icons and update CustomBlock component to use them

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
         'custom-cream': '#FEFAE0',
         'custom-amber': '#DDA15E',
         'custom-bronze': '#BC6C25',
+
+        'linkedin-blue': '#0A66C2',
+        'discord-purple': '#5865F2',
       },
       fontFamily: {
         'lostar': ['Lostar', 'sans-serif']
