bb3302829d61b4e7a44356f92b1cc830bae86a3b
Virgill_e <virgile.bigare@gmail.com>
2025-05-13 16:57:12 +0200

    install gsap

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="bg-custom-cream flex justify-around items-center p-4 text-custom-forest border-b-2 border-custom-olive text-xl">
         <a href="#" class="underline-gradient font-lostar">Virgill_e</a>
-        <div class="flex gap-8">
+        <div class="hidden md:flex gap-8">
             <a href="#" class="underline-gradient">Projects</a>
             <a href="#" class="underline-gradient">About</a>
             <a href="#" class="underline-gradient">CV</a>
         </div>
-        <a href="#" class="underline-gradient">Contact</a>
+        <a href="#" class="hidden md:block underline-gradient">Contact</a>
+        <div class="md:hidden">
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </button>
+        </div>
     </div>
 </template>
