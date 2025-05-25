6084badd9d52e30c03b4e2eeb711f1794de6aaa5
Virgill_e <virgile.bigare@gmail.com>
2025-05-25 20:13:23 +0200

    Update project header text for clarity: changed "What I can do" to "What I have already done" in projects.vue

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-screen bg-custom-cream py-16 px-8 md:px-24 flex flex-col items-center">
-        <h1 class="text-custom-forest text-6xl font-lostar">What I can do</h1>
+        <h1 class="text-custom-forest text-6xl font-lostar">What I have already done</h1>
 
         <UiProjectDescription class="py-16" />
         <UiProjectDescription class="py-16" />
