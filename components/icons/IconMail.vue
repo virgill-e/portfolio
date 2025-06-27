e702feaeb81f84cb95062e5810148b86f2e3f539
Virgill_e <virgile.bigare@gmail.com>
2025-06-27 17:22:03 +0200

    fix footer icons

--- components/icons/IconMail.vue
+++ components/icons/IconMail.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect x="2" y="4" width="20" height="16" rx="2" stroke="currentColor" stroke-width="2" fill="none" />
         <path d="M2 6L12 13L22 6" stroke="currentColor" stroke-width="2" stroke-linecap="round"
             stroke-linejoin="round" />
--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -6,20 +6,20 @@
         <span class="font-lostar text-3xl md:text-6xl text-center pointer-events-auto select-text">Create a project
             together</span>
         <div class="flex flex-row gap-4 md:gap-6 h-1/5 my-8 pointer-events-auto">
-            <a href="#" class="hover:opacity-75 transition-opacity w-8 h-8 md:w-10 md:h-10">
-                <IconsIconGithub class="w-full h-full" />
+            <a href="#" class="hover:opacity-75 transition-opacity block w-8 h-8 md:w-10 md:h-10">
+                <IconsIconGithub class="w-full h-full block" />
             </a>
-            <a href="#" class="hover:opacity-75 transition-opacity w-8 h-8 md:w-10 md:h-10">
-                <IconsIconLinkedin class="w-full h-full" />
+            <a href="#" class="hover:opacity-75 transition-opacity block w-8 h-8 md:w-10 md:h-10">
+                <IconsIconLinkedin class="w-full h-full block" />
             </a>
-            <a href="#" class="hover:opacity-75 transition-opacity w-8 h-8 md:w-10 md:h-10">
-                <IconsIconX class="w-full h-full" />
+            <a href="#" class="hover:opacity-75 transition-opacity block w-8 h-8 md:w-10 md:h-10">
+                <IconsIconX class="w-full h-full block" />
             </a>
-            <a href="#" class="hover:opacity-75 transition-opacity w-8 h-8 md:w-10 md:h-10">
-                <IconsIconInstagram class="w-full h-full" />
+            <a href="#" class="hover:opacity-75 transition-opacity block w-8 h-8 md:w-10 md:h-10">
+                <IconsIconInstagram class="w-full h-full block" />
             </a>
-            <a href="#" class="hover:opacity-75 transition-opacity w-8 h-8 md:w-10 md:h-10">
-                <IconsIconMail class="w-full h-full" />
+            <a href="#" class="hover:opacity-75 transition-opacity block w-8 h-8 md:w-10 md:h-10">
+                <IconsIconMail class="w-full h-full block" />
             </a>
         </div>
     </div>
