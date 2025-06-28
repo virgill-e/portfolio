aa8d5d955d5048cb7e2fbc37491978439f6db4b2
Virgill_e <virgile.bigare@gmail.com>
2025-06-28 11:08:56 +0200

    ajout d'un text-balance sur la paragraphe

--- components/ui/Button.vue
+++ components/ui/Button.vue
@@ -1,8 +1,10 @@
 <template>
     <button ref="buttonRef"
-        class="relative overflow-hidden  rounded-full border-2 text-custom-forest border-custom-forest uppercase transition-all duration-300 group">
+        class="relative overflow-hidden rounded-full border-2 text-custom-forest border-custom-forest uppercase transition-all duration-300 group flex items-center justify-center px-4 py-3 md:px-6 md:py-4 text-sm md:text-base">
         <span ref="fillRef" class="absolute left-0 top-0 h-full w-0 bg-custom-forest z-0 transition-none"></span>
-        <span class="relative z-10 group-hover:text-custom-cream transition-colors duration-300">{{ text }}</span>
+        <span
+            class="relative z-10 group-hover:text-custom-cream transition-colors duration-300 text-center leading-tight">{{
+            text }}</span>
     </button>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="min-h-screen bg-custom-cream px-16 md:px-24 relative z-25">
         <!-- title bloc -->
-        <div class="flex flex-col items-center md:justify-around h-[75vh] md:h-[90vh]">
+        <div class="flex flex-col items-center md:justify-around h-[90vh] pb-8">
             <div class="flex flex-col items-center justify-center h-3/5 md:h-3/5">
                 <h1 class="split font-lostar text-custom-cream text-shadow-custom-bronze text-6xl md:text-9xl ">
                     Virgill_e
@@ -11,8 +11,8 @@
                 </h1>
             </div>
             <div class="h-3/5 md:h-2/5 w-full flex flex-col md:flex-row justify-between md:items-center">
-                <p class="md:w-3/5 py-8  text-left md:text-lg">Hello, I'm Virgile Bigaré, a full stack developer
-                    passionate about creating web and mobile applications.<br>
+                <p class="md:w-3/5 py-8  text-left md:text-lg text-balance   ">Hello, I'm Virgile Bigaré, a full stack developer
+                    passionate about creating web and mobile applications.
                     Curious and always seeking new challenges, I enjoy exploring personal projects that
                     allow me to expand my skills and stay up-to-date in an ever-evolving field.</p>
                 <UiButton @click="scrollToUiProjectImageDisplays" class="py-4 md:w-1/3 md:h-1/3"
