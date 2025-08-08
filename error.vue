17f6d0c80037b88ee0b406a5dd9e13d30824e1d7
virgill_e <virgile.bigare@gmail.com>
2025-08-08 16:32:37 +0200

    fix bug 404

--- error.vue
+++ error.vue
@@ -1,21 +1,27 @@
 <template>
-    <div class="h-screen w-screen p-8 md:p-32 flex justify-center items-center">
+    <div class="page-404 h-screen w-screen p-8 md:p-32 flex justify-center items-center">
         <div class="bg-custom-amber rounded-full h-full w-full p-8 md:p-32 inset-shadow-deep border-amber">
             <div
                 class="bg-custom-bronze rounded-full h-full w-full flex flex-col justify-center items-center gap-8 inset-shadow-deep border-bronze">
-                <h1 class="text-custom-cream text-6xl md:text-9xl font-lostar text-center ">404</h1>
-                <NuxtLink to="/"
-                    class="bg-custom-cream text-custom-bronze px-6 py-3 rounded-full font-lostar text-lg md:text-xl hover:bg-custom-amber hover:text-custom-cream transition-colors duration-300">
+                <h1 class="text-custom-cream text-6xl md:text-9xl font-lostar text-center">404</h1>
+                <button type="button"
+                    class="bg-custom-cream text-custom-bronze px-6 py-3 rounded-full font-lostar text-lg md:text-xl hover:bg-custom-amber hover:text-custom-cream transition-colors duration-300"
+                    @click="handleGoHome">
                     Back Home
-                </NuxtLink>
+                </button>
             </div>
         </div>
     </div>
-
 </template>
 
-<style>
-* {
+<script setup lang="ts">
+const handleGoHome = () => {
+    clearError({ redirect: '/' });
+};
+</script>
+
+<style scoped>
+.page-404 * {
     cursor: default !important;
 }
 
