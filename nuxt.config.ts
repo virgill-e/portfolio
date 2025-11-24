ed9917643f73d3cb11641d47217c5d7511a08912
Virgill_e <virgile.bigare@gmail.com>
2025-11-24 21:13:09 +0100

    Integrate Nuxt Image for lazy loading and enhance font performance with preloading and display adjustments.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,26 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  ssr: false, // Pour une SPA statique
+
+  // Pour une SPA statique
+  ssr: false,
+
   css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      link: [
+        { rel: 'preload', href: '/fonts/Lostar.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' }
+      ]
+    }
+  },
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+
   nitro: {
     preset: 'node-server', // Pour le déploiement serveur
     port: process.env.PORT ? parseInt(process.env.PORT) : 3001,
@@ -17,10 +30,13 @@ export default defineNuxtConfig({
     // Configuration pour servir les assets statiques en mode SPA
     serveStatic: true,
   },
+
   // Configuration pour la production
   runtimeConfig: {
     public: {
       baseURL: process.env.BASE_URL || ''
     }
-  }
+  },
+
+  modules: ['@nuxt/image']
 });
